4299aa47f707cb5f63821c1924f075772e66ce72
juan <juancruz.23@hotmail.com>
2017-06-20 00:22:08 -0300

    postings are set in global memory during indexing, device can access them on query resolving time

--- GPU_server/init.cu
+++ GPU_server/init.cu
@@ -5,31 +5,50 @@
 
 #define POSTINGS_FILE "resources/seq_posting.txt"
 
+// global variables that are allocated in device during indexing
+Posting *d_postings;
+int *d_termsInPostings;
+
 // GPU KERNEL
-__global__ void k_resolveQuery (int *queryTerms, int querySize, float *docScores){
+__global__ void k_resolveQuery (
+		Posting *postings,
+		int *termsInPostings,
+		int *queryTerms,
+		int querySize,
+		float *docScores
+	){
 	int index = blockIdx.x * blockDim.x + threadIdx.x;
 	docScores[index] = index + 0.0f;
 	printf ("I am (%d, %d) with doc %d - score %1.1f\n", blockIdx.x, threadIdx.x, index, docScores[index]);
+	printf("\t Term %d is in %d docs\n", index, postings[index].docsLength);
+
 }
 
 void displayPosting(Posting *postings, int size);
 Posting* postingsFromSeqFile(FILE *postingsFile, int totalTerms);
 void index_collection();
-void resolve_query(char *query);
-
-
+void resolveQuery(char *query);
+void handleKernelError();
 
 
 int main(int argc, char const *argv[]) {
   index_collection();
 
+	/*
   char query[1000];
   printf("Enter query: ");
   fgets(query, 1000, stdin);
   if ((strlen(query)>0) && (query[strlen (query) - 1] == '\n'))
         query[strlen (query) - 1] = '\0';
-  resolve_query(query);
+  resolveQuery(query);
+ 	*/
+	char src[3], query[3];
 
+	strcpy(src,  "1 ");
+	strcpy(query, "2 ");
+
+	strcat(query, src);
+	resolveQuery(query);
   return 0;
 }
 
@@ -42,11 +61,26 @@ void index_collection() {
   }
   const int TERMS = 30332;
   Posting* postingsLoaded = postingsFromSeqFile(txtFilePtr, TERMS);
+
+	// Postings to device
+	printf("Copying postings from host to device\n");
+	int postingsSize = sizeof(Posting) * TERMS;
+	cudaMalloc((void **) &d_postings, postingsSize);
+	cudaMemcpy(d_postings, postingsLoaded, postingsSize, cudaMemcpyHostToDevice);
+
+	// terms to device
+	cudaMalloc((void **) &d_termsInPostings, sizeof(int));
+  cudaMemcpy(d_termsInPostings, &TERMS, sizeof(int), cudaMemcpyHostToDevice);
+
+	free(postingsLoaded);
   printf("Finish indexing\n");
 }
 
-void resolve_query(char *query){
+void resolveQuery(char *query){
   printf("Searching for: %s\n", query);
+	cudaEvent_t resolveQueryStart, resolveQueryStop;
+	cudaEventCreate(&resolveQueryStart);
+	cudaEventCreate(&resolveQueryStop);
   int i;
   int previousCharIsSpace = 0;
   int spacesCounter = 0;
@@ -73,29 +107,50 @@ void resolve_query(char *query){
   int *d_queryTerms;
 	float *docScores, *d_docScores;
 	int DOCS = 4;
-	int MAX_BLOCKS = 1024;
-	int MAX_THREADS = 1024;
-	int blocks = DOCS;
-	int threads = 1;
-	if (blocks > MAX_BLOCKS) {
-		blocks = MAX_BLOCKS;
-		printf("WARNING: too many documents in collection and not enough blocks. Only first 1024 docs will be processed\n");
-	}
+	int BLOCK_SIZE = DOCS;//1024;
+
+	int numBlocks = (DOCS + BLOCK_SIZE - 1) / BLOCK_SIZE;
 
 	docScores = (float *) malloc(sizeof(float) * DOCS);
 	cudaMalloc((void **) &d_queryTerms, querySize * sizeof(int));
   cudaMalloc((void **) &d_docScores, DOCS * sizeof(float));
 
   cudaMemcpy(d_queryTerms, queryTerms, querySize * sizeof(int), cudaMemcpyHostToDevice);
-	k_resolveQuery<<<blocks, threads>>>(d_queryTerms, querySize, d_docScores);
+
+	cudaEventRecord(resolveQueryStart);
+	k_resolveQuery<<<numBlocks, BLOCK_SIZE>>>(
+		d_postings,
+		d_termsInPostings,
+		d_queryTerms,
+		querySize,
+		d_docScores
+	);
+	handleKernelError();
+	cudaEventRecord(resolveQueryStop);
 
 	cudaMemcpy(docScores, d_docScores, DOCS * sizeof(float), cudaMemcpyDeviceToHost);
 
+	cudaEventSynchronize(resolveQueryStop);
+	float milliseconds = 0;
+	cudaEventElapsedTime(&milliseconds, resolveQueryStart, resolveQueryStop);
+
+	/*
 	for (i = 0; i < DOCS; i++) {
 		printf("doc %d: %1.1f\n", i, docScores[i]);
 	}
-	
+	*/
+	printf("Time elapsed: %10.4f ms\n", milliseconds);
+
 	cudaFree(d_queryTerms);
 	cudaFree(d_docScores);
   free(queryTerms);
+}
+
+void handleKernelError(){
+	cudaError_t errSync  = cudaGetLastError();
+	cudaError_t errAsync = cudaDeviceSynchronize();
+	if (errSync != cudaSuccess)
+	  printf("Sync kernel error: %s\n", cudaGetErrorString(errSync));
+	if (errAsync != cudaSuccess)
+	  printf("Async kernel error: %s\n", cudaGetErrorString(errAsync));
 }
