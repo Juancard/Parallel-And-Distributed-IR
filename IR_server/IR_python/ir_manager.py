163e8f2f9e2b1cba598c27f8d18aa378342959ef
Juan Cardona <juancruz.23@hotmail.com>
2017-09-15 23:02:21 -0300

    python server generate data structures to prepare for retrieval tasks

--- IR_server/IR_python/ir_manager.py
+++ IR_server/IR_python/ir_manager.py
@@ -4,9 +4,13 @@ import logging
 import argparse
 import ConfigParser
 import struct
+import collections
+import pprint
+import numpy as np
 from modulos.Collection import Collection
 from modulos.Indexer import Indexer
 from custom_exceptions import NoIndexFilesException
+
 
 POSTINGS_FILENAME = "postings.bin"
 MAXFREQS_FILENAME = "max_freq_in_docs.bin"
@@ -17,13 +21,15 @@ class IRManager(object):
 
     def __init__(self):
         self.indexPath = loadIni()["index_dir"]
+        self.corpusPath = False
         self.docs = False
         self.terms = False
         self.vocabulary = False
         self.documents = False
         self.postings = False
         self.maxFreqInDocs = False
-        self.corpusPath = False
+        self.idf = False
+        self.docsNorm = False
 
     def index(self, corpusPath):
     	try:
@@ -46,6 +52,7 @@ class IRManager(object):
         self.maxFreqInDocs = indexer.maxFreqInDocs
         self.terms = len(self.vocabulary.content)
         self.docs = len(self.documents.content)
+        self.setRetrievalData()
 
     def evaluate(self, query):
         docScores = {}
@@ -54,7 +61,7 @@ class IRManager(object):
         return docScores
 
     def loadStoredIndex(self):
-        print "Loading stored index"
+        logging.info("Loading stored index")
 
         if not os.path.isdir(self.indexPath):
             logging.info("Index files directory does not exist. Creating...")
@@ -67,6 +74,33 @@ class IRManager(object):
             raise NoIndexFilesException("index files have not been generated.")
 
         metadata = loadMetadata(metadataDir)
+        self.docs = metadata["docs"]
+        self.terms = metadata["terms"]
+        self.maxFreqInDocs = loadMaxfreqs(maxFreqsDir, self.docs)
+        df = loadDf(pointersDir, self.terms)
+        self.postings = loadPostings(postingsDir, df)
+
+        logging.info("Generating retrieval data structures")
+        self.setRetrievalData()
+
+    def setRetrievalData(self):
+        self.docsNorm = {}
+        for dId in range(0, self.docs):
+            self.docsNorm[dId] = 0.0
+
+        self.idf = {}
+        for tId in self.postings:
+            df = len(self.postings[tId]) + 0.0
+            currentIdf = np.log10(self.docs / df)
+            self.idf[tId] = currentIdf
+            for dId in self.postings[tId]:
+                currentTf = (self.postings[tId][dId] + 0.0) / self.maxFreqInDocs[dId]
+                currentTfIdf = currentTf * currentIdf
+                self.postings[tId][dId] = currentTfIdf
+                self.docsNorm[dId] += currentTfIdf ** 2.0
+
+        for dId in self.docsNorm:
+            self.docsNorm[dId] = self.docsNorm[dId] ** 0.5
 
 def loadIni():
 	INI_PATH = os.path.dirname(os.path.realpath(__file__)) + "/config.ini"
@@ -88,3 +122,32 @@ def loadMetadata(metadataDir):
         metadata["docs"] = meta[0]
         metadata["terms"] = meta[1]
     return metadata
+
+def loadMaxfreqs(metadataDir, docs):
+    maxfreqs = {}
+    with open(metadataDir, "rb") as f:
+        mfRead = f.read(docs * 4)
+        mfRead = struct.unpack('<%iI' % docs, mfRead)
+        for docId in range(0, docs):
+            maxfreqs[docId] = int(mfRead[docId])
+    return maxfreqs
+
+def loadDf(dfDir, terms):
+    df = {}
+    with open(dfDir, "rb") as f:
+        read = f.read(terms * 4)
+        read = struct.unpack('<%iI' % terms, read)
+        for tId in range(0, terms):
+            df[tId] = int(read[tId])
+    return df
+
+def loadPostings(postingsDir, df):
+    postings = collections.OrderedDict()
+    with open(postingsDir, "rb") as f:
+        for tId in range(0, len(df)):
+            postings[tId] = collections.OrderedDict()
+            docIdsRead = struct.unpack('<%iI' % df[tId], f.read(df[tId] * 4))
+            freqsRead = struct.unpack('<%iI' % df[tId], f.read(df[tId] * 4))
+            for i in range(0, df[tId]):
+                postings[tId][int(docIdsRead[i])] = int(freqsRead[i])
+    return postings
